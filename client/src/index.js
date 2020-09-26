e674d680766a2b20a724a3558a8e0583deee610c
Scott Blanch <scottglenblanch@gmail.com>
2020-09-25 18:56:54 -0600

    inital commit

---
+++ client/src/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/api', (req, res) => {
+  res.json({
+
+  });
+
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+});
